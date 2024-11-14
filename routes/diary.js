415f7ba464594d1e33e08e7de1b9b7b3d3c3d112
hesuhesu <zndwk0728@gmail.com>
2024-11-15 00:45:53 +0900

    Add Diary Collections

---
+++ routes/diary.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const Diary = require('../models/Diary');
+
+router.get('/', (req, res) => {
+    res.send('다이어리 가능');
+});
+
+// 글 하나 조회
+router.get("/read", async (req, res) => {
+    try {
+        const diary = await Diary.findOne({ _id : req.query._id });
+        res.json({ list: diary });
+    } catch (err) {
+        res.json({ message: false });
+    }
+});
+
+// 글 전체 목록 조회
+router.get("/all_read", async(req, res) => {
+    try {
+        const diary = await Diary.find().sort({ createdAt: -1 });
+        console.log("데이터 보냄");
+        res.json({ list: diary });
+    } catch (error) {
+        console.log(error);
+        res.json({ message: false });
+    }
+});
+
+// 글 쓰기
+router.post("/write", async(req, res) => {
+    try {
+      let obj;
+      const now = new Date();
+      const createdAt = now.toLocaleString('ko-KR', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              hour12: false
+      });
+      obj = {
+        title: req.body.title,
+        content: req.body.content,
+        createdAt: createdAt
+      };
+      const diary = new Diary(obj);
+      await diary.save();
+      res.json({ message: "게시글이 업로드 되었습니다." });
+    } catch (error) {
+        console.log(error);
+        res.json({ message: false });
+    }
+})
+
+// 글 업데이트
+router.put("/update", async (req,res) => {
+    try {
+        
+        await Board.updateOne(
+            {_id: req.body._id},
+            {
+                $set: {
+                    title: req.body.title,
+                    content: req.body.content,
+                }
+            }
+        );
+        res.json({ message : "게시글이 수정되었습니다." })
+    } catch (error) {
+        res.json({ message: false });
+    }
+});
+
+// 글 삭제 
+router.delete("/delete", async(req,res) => {
+    try {
+       await Board.deleteOne({_id: req.query._id })
+       res.json({ message: true });
+    } catch (error) {
+       res.json({ message: false });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const path = require('path');
 const fs = require('fs');
 const https = require('https');
 const reviewRoute = require('./routes/review');
+const diaryRoute = require('./routes/diary');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const MONGO_PORT = process.env.MONGO_PORT;
+const AUTH_USER = process.env.AUTH;
+const AUTH_PASSWORD = process.env.AUTH_PASSWORD;
+const READ_WRITE_USER= process.env.READ_WRITE_USER;
+const READ_WRITE_PASSWORD = process.env.READ_WRITE_PASSWORD;
 
 // Middleware
 app.use(express.json());
@@ -26,6 +32,7 @@ app.get('/', (req, res) => {
 
 // Routes Middleware
 app.use('/review', reviewRoute);
+app.use('/diary', diaryRoute);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -35,6 +42,43 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Failed to connect to MongoDB', err);
+});
+
+function connectToDatabase(username, password) {
+  const uri = `mongodb://${username}:${password}@127.0.0.1:${MONGO_PORT}`;
+  if (mongoose.connection.readyState === 1) { mongoose.disconnect(); }
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => { console.log('Connected to MongoDB'); })
+      .catch(err => { console.error('MongoDB connection error:', err); });
+}
+
+// 로그인 엔드포인트
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // 사용자 인증 로직
+  if (username === AUTH_USER && password === AUTH_PASSWORD) {
+      connectToDatabase(READ_WRITE_USER, READ_WRITE_PASSWORD);
+      return res.status(200).send('Login successful');
+  } else {
+    if (mongoose.connection.readyState === 1) {
+      mongoose.disconnect();
+      return res.status(200).send('logout');;
+    }
+    return res.status(401).send('Invalid credentials');
+  }
+});
+
+// 로그아웃 엔드포인트
+app.post('/logout', (req, res) => {
+  if (mongoose.connection.readyState === 1) {
+      mongoose.disconnect();
+      currentUser = null; // 현재 사용자 정보 초기화
+      console.log('Logged out successfully');
+      return res.status(200).send('Logout successful');
+  } else {
+      return res.status(400).send('No user is logged in');
+  }
 });
 
 app.listen(PORT, () => {
